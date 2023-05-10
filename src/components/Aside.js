03b92ab6d9e62fe334e7701ceb2e689ccea657e3
Jasson Fernandez <jeicobfer@gmail.com>
2023-05-10 09:57:20 -0600

    implementando la plantilla del panel admin a proyecto de react

---
+++ src/components/Aside.js
@@ -0,0 +1,146 @@
+import React from 'react'
+
+export default function Aside() {
+  return (
+    <aside className="main-sidebar sidebar-dark-primary elevation-4">
+      {/* Brand Logo */}
+      <a href="index3.html" className="brand-link">
+        <img
+          src="dist/img/AdminLTELogo.png"
+          alt="AdminLTE Logo"
+          className="brand-image img-circle elevation-3"
+          style={{ opacity: ".8" }}
+        />
+        <span className="brand-text font-weight-light">InventMED</span>
+      </a>
+      {/* Sidebar */}
+      <div className="sidebar">
+        {/* Sidebar user panel (optional) */}
+        <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div className="image">
+            <img
+              src="dist/img/user2-160x160.jpg"
+              className="img-circle elevation-2"
+              alt="User Image"
+            />
+          </div>
+          <div className="info">
+            <a href="#" className="d-block">
+              Jasson Fernandez
+            </a>
+          </div>
+        </div>
+        {/* SidebarSearch Form */}
+        <div className="form-inline">
+          <div className="input-group" data-widget="sidebar-search">
+            <input
+              className="form-control form-control-sidebar"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <div className="input-group-append">
+              <button className="btn btn-sidebar">
+                <i className="fas fa-search fa-fw" />
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* Sidebar Menu */}
+        <nav className="mt-2">
+          <ul
+            className="nav nav-pills nav-sidebar flex-column"
+            data-widget="treeview"
+            role="menu"
+            data-accordion="false"
+          >
+            {/* Add icons to the links using the .nav-icon class
+         with font-awesome or any other icon font library */}
+            <li className="nav-item menu-open">
+              <a href="#" className="nav-link active">
+                <i className="nav-icon fas fa-tachometer-alt" />
+                <p>
+                  Dashboard
+                  <i className="right fas fa-angle-left" />
+                </p>
+              </a>
+              <ul className="nav nav-treeview">
+                <li className="nav-item">
+                  <a href="./index3.html" className="nav-link active">
+                    <i className="far fa-circle nav-icon" />
+                    <p>Register</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-header">INFORMACION</li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                <i className="nav-icon fas fa-table" />
+                <p>
+                  Tables
+                  <i className="fas fa-angle-left right" />
+                </p>
+              </a>
+              <ul className="nav nav-treeview">
+                <li className="nav-item">
+                  <a href="pages/tables/jsgrid.html" className="nav-link">
+                    <i className="far fa-circle nav-icon" />
+                    <p>Medical</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-header">INFORMES</li>
+            <li className="nav-item">
+              <a href="pages/calendar.html" className="nav-link">
+                <i className="nav-icon fas fa-calendar-alt" />
+                <p>
+                  Calendar
+                  <span className="badge badge-info right">2</span>
+                </p>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="pages/gallery.html" className="nav-link">
+                <i className="nav-icon far fa-image" />
+                <p>Gallery</p>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                <i className="nav-icon far fa-envelope" />
+                <p>
+                  Mailbox
+                  <i className="fas fa-angle-left right" />
+                </p>
+              </a>
+              <ul className="nav nav-treeview">
+                <li className="nav-item">
+                  <a href="pages/mailbox/mailbox.html" className="nav-link">
+                    <i className="far fa-circle nav-icon" />
+                    <p>Inbox</p>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="pages/mailbox/compose.html" className="nav-link">
+                    <i className="far fa-circle nav-icon" />
+                    <p>Compose</p>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="pages/mailbox/read-mail.html" className="nav-link">
+                    <i className="far fa-circle nav-icon" />
+                    <p>Read</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        {/* /.sidebar-menu */}
+      </div>
+      {/* /.sidebar */}
+    </aside>
+  );
+}
---
+++ src/components/Conten.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Conten() {
+  return (
+    <div className="content-wrapper">
+        <h1>Este es todo el contenido</h1>
+    </div>
+  )
+}
